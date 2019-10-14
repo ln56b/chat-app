301de4bfda9f72cda81a4f1d7c7500a95d33a3ef
Hélène Baudrand <baudrand.helene@gmail.com>
2019-10-14 17:56:52 +0200

    Changed contact to include state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact (props) {
-    return (
-    <figure className = "Contact">
-        <img className = "avatar" src = {props.image} alt = {props.name} />
-        <figcaption>
-            <blockquote className = "name"> 
-            {props.name}
-            </blockquote>
-            <div className = "status">
-                <cite className = {props.online? "status-online" : "status-offline"}>
-                </cite>
-                <cite className = "status-text">
-                {props.online ? "online" : "offline"}
-                </cite>
+class Contact extends React.Component {
 
-            </div>
-
-        </figcaption>
-    </figure>
-
-    );
-
+    constructor(props){
+        super(props);
+        this.state = {
+            online : false,
+        };
+    }
+    render() {
+        return (
+        <figure className = "Contact">
+            <img className = "avatar" src = {this.props.image} alt = {this.props.name} />
+            <figcaption>
+                <blockquote className = "name"> 
+                {this.props.name}
+                </blockquote>
+                <div className = "status"
+                onClick = {event => {
+                    const newStatus = !this.state.online;
+                    this.setState({online: newStatus});
+                }}
+                >
+                    <cite 
+                    className = {this.state.online? "status-online" : "status-offline"}
+                    
+                    >
+                    </cite>
+                    <cite 
+                    className = "status-text"
+                    >
+                    {this.state.online ? "online" : "offline"}
+                    </cite>
+                </div>
+            </figcaption>
+        </figure>
+        );
+    }
 }
 
 export default Contact;
